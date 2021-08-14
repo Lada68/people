19554d69309751f0371d67b189d3c0d582edd1da
Lada Antsifirova <dusya629@gmail.com>
2021-08-15 00:08:58 +0300

    add main.kt

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var likes = 143871
+
+    if (likes % 10 == 1 && likes != 11) {
+        println("Понравилось $likes человеку")
+    } else
+        println("Понравилось $likes людям")
+}
+
