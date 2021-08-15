7cc7a98a74a6027a683645b346ffe16004646246
Lada Antsifirova <dusya629@gmail.com>
2021-08-15 20:25:40 +0300

    change in main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
     var likes = 143871
 
-    if (likes % 10 == 1 && likes != 11) {
+    if (likes % 10 == 1 && likes % 100 != 11) {
         println("Понравилось $likes человеку")
     } else
         println("Понравилось $likes людям")
